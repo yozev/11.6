370a87f935a4d7bfa78fa4d9790a33baf78f594f
Artur Tulibaev <yozevarturgm@gmail.com>
2024-10-22 20:40:29 +0300

    13.6.2

--- 13/13.6.2/13.6.2.cpp
+++ 13/13.6.2/13.6.2.cpp
@@ -1,1 +1,23 @@
 ﻿
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<float> prices {1, 2, 3, 4, 5, 6, 7};
+	std::vector<int> items {0,1,2,6};
+	float sum = 0;
+	int issue = 0;
+	for (int i = 0; i < items.size(); i++) {
+		if (items[i] < prices.size()) {
+			sum += prices[items[i]];
+		}
+		else {
+			std::cout << "error (items index issues) in " << items[i] << " in " << i << " index" << std::endl;
+			issue++;
+		}
+	}
+	if (issue == 0)
+		std::cout << "summ is: " << sum << "size is " << prices.size();
+
+}
