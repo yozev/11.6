babdcfd6d3651bcc5593e1f88199882b542d5045
Artur Tulibaev <yozevarturgm@gmail.com>
2024-10-22 21:47:57 +0300

    13.6.3

---
+++ 13/13.6.3/13.6.3/13.6.3.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> db(20);
+    int number;
+    int i = 0;
+    std::cout << "input number: ";
+    std::cin >> number;
+    while (number != -1)
+    {
+        if (i == db.size()) {
+            
+            for (int j = 0; j < db.size() - 1; j++)
+            {
+                db[j] = db[j+1];
+            }
+            db[db.size() - 1] = number;
+            i--;
+        }
+        else if (i < db.size()) {
+            db[i] = number;
+        }
+        i++;
+        std::cout << "input number: ";
+        std::cin >> number;
+    }
+    for (int i = 0; i < db.size(); i++) {
+        std::cout << db[i] << " ";
+    }
+}
